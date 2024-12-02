ad3a3f413831439f3c647b83f270fedfb74e2b03
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-12-02 11:08:59 +0100

    solved: Day 1 Part 2

--- advent-of-code-2024/lib/day01/day01.dart
+++ advent-of-code-2024/lib/day01/day01.dart
@@ -27,3 +27,41 @@ int partA(){
 
   return response;
 }
+
+int partB(){
+
+  int response = 0;
+
+  List<String> lines = obtainFileLines('lib/day01/input.txt');
+
+  List<int> listOne = [];
+  List<int> listTwo = [];
+
+  for(String line in lines){
+
+    int firstSpace = line.indexOf(' ');
+    int lastSpace = line.lastIndexOf(' ');
+    listOne.add(int.parse(line.substring(0, firstSpace)));
+    listTwo.add(int.parse(line.substring(lastSpace + 1, line.length)));
+  }
+  Map<int, int> map = {};
+  for(int i = 0; i < listOne.length; i++){
+
+    map.putIfAbsent(listOne[i], () => 0);
+  }
+
+  for(int i = 0; i < listTwo.length; i++){
+    
+    int number = listTwo[i];
+    
+    if(map.containsKey(number)){
+      map.update(number, (value) => value + 1);
+    }
+  }
+
+  map.forEach((key, value) {
+    response += key * value;
+  });
+
+  return response;
+}
